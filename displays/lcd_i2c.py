8d1abb156d786086d83804b3a9cbf728587d1e92
Brandon Smith Rapalino <brandon.rapalino@gse.com.co>
2025-08-11 13:16:39 -0500

    ajuste codigo de ejemplo i2c display

--- displays/lcd_i2c.py
+++ displays/lcd_i2c.py
@@ -1,29 +1,13 @@
 from RPLCD.i2c import CharLCD
-from time import sleep
+import time
 
-# Inicializa el LCD en la dirección I2C 0x27 (cambia si es necesario)
-lcd = CharLCD('PCF8574', 0x27, cols=16, rows=2)
+# Ajusta la dirección I²C según el escaneo
+lcd = CharLCD('PCF8574', 0x27)  # 0x27 o 0x3F
 
-try:
-    # Mostrar mensaje en la primera línea
-    lcd.write_string("Maleta Interactiva")
-    sleep(2)
-    
-    # Borrar y escribir en dos líneas
-    lcd.clear()
-    lcd.write_string("Raspberry Pi 5")
-    lcd.crlf()  # Salto a la segunda línea
-    lcd.write_string("LCD 16x2 I2C")
-    sleep(5)
+lcd.clear()
+lcd.write_string("Hola Raspberry Pi")
+lcd.crlf()  # Salto de línea
+lcd.write_string("LCD 16x2 I2C")
 
-    # Mensaje final con scroll simple
-    lcd.clear()
-    mensaje = "Proyecto de grado "
-    for i in range(len(mensaje)):
-        lcd.clear()
-        lcd.write_string(mensaje[i:] + mensaje[:i])
-        sleep(0.3)
-
-finally:
-    lcd.clear()
-    lcd.close(clear=True)
+time.sleep(5)
+lcd.clear()
